2162e4a69a3948bcfdd9772d73f57c3d0468495b
Beaton <69068301+Paramee0598@users.noreply.github.com>
2021-11-20 19:27:45 +0700

    Add files via upload

---
+++ LinearPython/MCov.py
@@ -0,0 +1,59 @@
+import csv
+import numpy as np
+
+with open('data/covAllcal.csv','r') as f:
+    reader = csv.reader(f,delimiter = ",")
+    i = list(reader)
+x = np.array(i).astype('int')
+
+print('matrix x')
+print(x)
+print('-------')
+
+print('size of x (rows,colums)')
+print(x.shape)  #rows,colums
+print('-------')
+
+xDimention = x.shape[0]
+print('rows')
+print(xDimention)
+print('-------')
+
+oneM= np.full((xDimention,xDimention), 1) # make One Square Matrix
+print('one of matrix')
+print(oneM)
+print('-------')
+
+xb = (1/int(xDimention))*np.dot(oneM,x) # Xbar
+print("Xbar")
+print(xb)
+print('-------')
+
+# x - xbar
+x_xb = x - xb
+print('x-xbar')
+print(x_xb)
+print('-------')
+
+x_xbT = x_xb.T
+print('x_xbT')
+print(x_xbT)
+print('-------')
+
+covX = (1/int(xDimention))*np.dot(x_xbT,x_xb)
+print('covariance matrix')
+print(covX)
+print('-------')
+
+coverCovX = str(covX)
+coverCovX = coverCovX.split(" ")
+np.savetxt("data/CovFinishAll.csv", covX, delimiter=",",fmt='%.2f')
+filename = 'data/covAllcal.csv'
+
+
+
+
+
+
+
+
